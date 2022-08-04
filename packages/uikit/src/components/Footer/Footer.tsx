eac9dcb778633a1beec2a9c2ea2e768f687f0236
jjf7 <jfsistemas.contacto@gmail.com>
2022-08-04 15:49:54 -0400

    first commit

---
+++ packages/uikit/src/components/Footer/Footer.tsx
@@ -0,0 +1,38 @@
+import React from "react";
+import { baseColors, darkColors, lightColors } from "../../theme/colors";
+import { Flex, Box } from "../Box";
+import { Link } from "../Link";
+import {
+  StyledFooter,
+  StyledIconMobileContainer,
+  StyledList,
+  StyledListItem,
+  StyledText,
+  StyledSocialLinks,
+  StyledToolsContainer,
+} from "./styles";
+import { FooterProps } from "./types";
+import { ThemeSwitcher } from "../ThemeSwitcher";
+import LangSelector from "../LangSelector/LangSelector";
+import CakePrice from "../CakePrice/CakePrice";
+import { LogoWithTextIcon, ArrowForwardIcon } from "../Svg";
+import { Button } from "../Button";
+import { Colors } from "../..";
+
+const MenuItem: React.FC<FooterProps> = ({
+  items,
+  isDark,
+  toggleTheme,
+  currentLang,
+  langs,
+  setLang,
+  cakePriceUsd,
+  buyCakeLabel,
+  ...props
+}) => {
+  return (
+    <></>
+  );
+};
+
+export default MenuItem;
